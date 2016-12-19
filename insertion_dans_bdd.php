1bdaa4a22e8e18af4e52c18da746e0050cd0e89f
salis <salis.garba1@gmail.com>
2016-12-19 23:47:34 +0100

    Renommage de variable

--- insertion_dans_bdd.php
+++ insertion_dans_bdd.php
@@ -10,15 +10,15 @@ if (($handle = fopen("tickets_appels_201202.csv", "r")) !== FALSE) {
   // Insertion des informations dans la base de données.
   for($i=3;$i<sizeof($donees); $i++)
   {
-    $ligne = explode(";",$donees[$i][0]);
-    $ligne[7] = addslashes($ligne[7]);
+    $info = explode(";",$donees[$i][0]);
+    $info[7] = addslashes($info[7]);
 
     // Adaptation du format de la date à celui de MySQL.
-    $temp =  explode("/",$ligne[3]);
-    $ligne_date = $temp[2].'-'.$temp[1].'-'.$temp[0];
+    $temp =  explode("/",$info[3]);
+    $info_date = $temp[2].'-'.$temp[1].'-'.$temp[0];
 
     $sql ="INSERT INTO tickets(numeroAbonne,laDate,heure,dureeReel,dureeFacture,type)
-    VALUES ('".$ligne[2]."','".$ligne_date."','".$ligne[4]."','".$ligne[5]."','".$ligne[6]."','".$ligne[7]."')";
+    VALUES ('".$info[2]."','".$info_date."','".$info[4]."','".$info[5]."','".$info[6]."','".$info[7]."')";
     mysql_query($sql) or die("enchant_dict_get_error ". mysql_error());
 
   }
